aed9556ae619f43d860ebce50cbd4c454ede3cd6
Jake Whitton <jwhitton@mit.edu>
2019-08-05 18:02:54 -0700

    Change wm to exwm

--- universal/home/jake/.emacs.d/init.el
+++ universal/home/jake/.emacs.d/init.el
@@ -2,3 +2,20 @@
 (org-babel-load-file
   (expand-file-name "settings.org"
 		    user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+	("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+	(xelb exwm eyebrowse use-package spacemacs-theme solarized-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
